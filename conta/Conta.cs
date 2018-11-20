52ab769e2ec33c1792073e5423a10a7a1353cf75
Marcus Vinicius Sobral <mvinicius.sobral@gmail.com>
2018-11-20 14:21:15 -0200

    classe Conta criada com metodo saque e deposito

---
+++ conta/Conta.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace conta
+{
+    class Conta
+    {
+        public int numero { get; set; }
+        public string titular { get; set; }
+        public double saldo { get; private set; }
+        public double limiteDeDaque { get; set; }
+
+        public Conta(int numero, string titular, double limiteDeSaque)
+        {
+            this.numero = numero;
+            this.titular = titular;
+            this.limiteDeDaque = limiteDeDaque;
+            this.saldo = 0.0;
+        }
+
+        public void depositar(double valor)
+        {
+            saldo = saldo + valor;
+        }
+
+        public void sacar(double valor)
+        {
+            saldo = saldo - valor;
+        }
+    }
+}
