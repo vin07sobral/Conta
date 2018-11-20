124832b3c26ea3e34c7dc7a69ca15327b9b25ff5
Marcus Vinicius Sobral <mvinicius.sobral@gmail.com>
2018-11-20 14:31:57 -0200

    Entrada de dados adicionada, chamada dos metodos para saque e deposito

--- conta/Program.cs
+++ conta/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace conta
 {
@@ -6,7 +7,26 @@ namespace conta
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Nome do titular: ");
+            string titular = Console.ReadLine();
+            Console.Write("Limite de saque: ");
+            double limite = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Conta C = new Conta(numero, titular, limite);
+
+            Console.WriteLine();
+            Console.Write("Informe um valor para depósito: ");
+            double valorDeposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            C.depositar(valorDeposito);
+            Console.WriteLine("Novo saldo = R$: " + C.saldo.ToString("F2,", CultureInfo.InvariantCulture));
+
+            Console.WriteLine();
+            Console.Write("Informe um valor para saque: ");
+            double valorSaque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            C.sacar(valorSaque);
+            Console.WriteLine("Novo saldo = R$: " + C.saldo.ToString("F2", CultureInfo.InvariantCulture));
         }
     }
 }
